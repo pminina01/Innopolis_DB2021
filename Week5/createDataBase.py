3ee0100d824951e7430e93c735783428ef10c90c
pminina01 <35624330+pminina01@users.noreply.github.com>
2021-02-23 02:25:06 +0300

    Add files via upload

--- Week5/createDataBase.py
+++ Week5/createDataBase.py
@@ -1,19 +1,24 @@
 from faker import Faker
 import psycopg2
 
-con = psycopg2.connect(database="customers", user="postgres", password="8951", host="localhost", port="5432")
-print("Database is opened")
+con = psycopg2.connect(database="persons",
+                       user="postgres", password="8951",
+                       host="localhost", port="5432")
+print("Database persons is opened")
 cur = con.cursor()
-cur.execute('''CREATE TABLE CUSTOMER (ID INT PRIMARY KEY NOT NULL,
+cur.execute('''CREATE TABLE PERSON (ID INT PRIMARY KEY NOT NULL,
                                       Name TEXT NOT NULL,
                                       Address TEXT NOT NULL,
+                                      age INT,
                                       review TEXT);''')
-print("Table is created successfully")
+print("Table PERSON was created successfully")
 fake = Faker()
 for i in range(100000):
     print("#"+str(i))
-    cur.execute("INSERT INTO CUSTOMER (ID,Name,Address,review) VALUES ('" +
+    cur.execute("INSERT INTO PERSON (ID,Name,Address, age, review) VALUES ('" +
                 str(i) + "','" + fake.name() + "','" +
-                fake.address() + "','" + fake.text() + "')")
+                fake.address() + "','" + str(fake.random_int(1, 120)) +  "','" + fake.text() + "')")
     con.commit()
 print("Finished")
+
+
